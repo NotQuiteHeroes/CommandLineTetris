914e1588710c3cb27a94f714e0d3c84863504c3c
NotQuiteHeroes <paige.eckstein@gmail.com>
2018-02-08 14:55:44 -0600

    Update Paths

--- allPieces.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ALLPIECES_
-#define _ALLPIECES_
-
-extern char allPieces[7][4][5][5];
-
-#endif
--- main.cpp
+++ main.cpp
@@ -1,57 +1,57 @@
-#include "Gameplay.h"
-#include "Board.h"
-#include "Piece.h"
+#include "headers\Gameplay.h"
+#include "headers\Board.h"
+#include "headers\Piece.h"
 
-#include<ctime>
-#include<cstdlib>
-#include<iostream>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
 
 int main()
 {
-	Gameplay gameplay;
-	Piece piece;
-	Board board;
-	
-	bool gameOver = false;
-	bool clearLines = false;
-	
-	std::srand(std::time(0));
-	
-	int randomShape = std::rand() % 7;
-	piece.setCurrentShape(randomShape);
-	piece.setCurrentRotation(0);
-	
-	do
-	{
-		board.updateBoard(piece);
-		
-		board.displayBoard();
-		std::cout<<"Points: "<<gameplay.getPoints()<<std::endl;
-		
-		int move = gameplay.getMovement();
-		bool validMove = gameplay.validMove(board, piece, move);
-		
-		
-		if(validMove == true)
-		{
-			board.updateBoard(piece);
-			
-		}
-		else if(piece.getPieceIsStuck() == true)
-		{
-			gameplay.getNextPiece(piece);
-			clearLines = board.checkBoard();
-		
-			if(clearLines)
-			{
-				gameplay.addPoints();
-			}
-		
-		}
-		
-		gameOver = gameplay.gameOver(board);
-		
-	} while(gameOver != true);
-	
-	std::cout<<"Game Over!"<<std::endl;
+								Gameplay gameplay;
+								Piece piece;
+								Board board;
+
+								bool gameOver = false;
+								bool clearLines = false;
+
+								std::srand(std::time(0));
+
+								int randomShape = std::rand() % 7;
+								piece.setCurrentShape(randomShape);
+								piece.setCurrentRotation(0);
+
+								do
+								{
+																board.updateBoard(piece);
+
+																board.displayBoard();
+																std::cout<<"Points: "<<gameplay.getPoints()<<std::endl;
+
+																int move = gameplay.getMovement();
+																bool validMove = gameplay.validMove(board, piece, move);
+
+
+																if(validMove == true)
+																{
+																								board.updateBoard(piece);
+
+																}
+																else if(piece.getPieceIsStuck() == true)
+																{
+																								gameplay.getNextPiece(piece);
+																								clearLines = board.checkBoard();
+
+																								if(clearLines)
+																								{
+																																gameplay.addPoints();
+																								}
+
+																}
+
+																gameOver = gameplay.gameOver(board);
+
+								} while(gameOver != true);
+
+								std::cout<<"Game Over!"<<std::endl;
 }
